0d521a4fd6741a9cf8419735793d0224ee4fd808
Adrián <sirk8989@gmail.com>
2025-11-20 21:11:59 +0100

    Auto stash before merge of "Backend" and "origin/Backend"

--- frontFlixScore/front_flixscore/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ frontFlixScore/front_flixscore/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
+<<<<<<< Updated upstream
 export "FLUTTER_ROOT=/Users/opahostil/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/opahostil/Documents/ProyectoDAM/proyectoTFG/frontFlixScore/front_flixscore"
+=======
+export "FLUTTER_ROOT=C:\Users\adrid\OneDrive\Documentos\Proyectos_Flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\adrid\OneDrive\Documentos\GitHub\proyectoTFG\frontFlixScore\front_flixscore"
+>>>>>>> Stashed changes
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
